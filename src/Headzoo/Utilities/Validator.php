075880f7afd7083f047fa44879817f64e5b4623f
headzoo <sean@headzoo.io>
2014-03-25 01:55:16 -0400

    Created ValidatorInterface::validateRequired()

--- src/Headzoo/Utilities/Validator.php
+++ src/Headzoo/Utilities/Validator.php
@@ -30,23 +30,9 @@ class Validator
         
         return $this;
     }
-    
+
     /**
-     * Throws an exception when required values are missing from an array of key/value pairs
-     * 
-     * The $values argument is an array of key/value pairs, and the $required argument is an array
-     * of keys which must exist in $values to validate. When $allowEmpty is false, the required values
-     * must also evaluate to a non-empty value to validate.
-     * 
-     * Use the Validator::setThrownException() method to set which type of exception is thrown.
-     * 
-     * This method always returns true, but throws an exception when the value is invalid.
-     * 
-     * @param  array $values       The values to validate
-     * @param  array $required     List of keys
-     * @param  bool  $allowEmpty   Are empty values acceptable?
-     * @return bool
-     * @throws Exception When a required value is missing
+     * {@inheritDoc}
      */
     public function validateRequired(array $values, array $required, $allowEmpty = false)
     {
--- src/Headzoo/Utilities/ValidatorInterface.php
+++ src/Headzoo/Utilities/ValidatorInterface.php
@@ -1,6 +1,7 @@
 <?php
 namespace Headzoo\Utilities;
 use InvalidArgumentException;
+use Exception;
 
 /**
  * Interface for classes which perform data validation.
@@ -20,4 +21,23 @@ interface ValidatorInterface
      * @throws InvalidArgumentException When $thrownException does not name a sub-class of Exception
      */
     public function setThrownException($thrownException);
+
+    /**
+     * Throws an exception when required values are missing from an array of key/value pairs
+     *
+     * The $values argument is an array of key/value pairs, and the $required argument is an array
+     * of keys which must exist in $values to validate. When $allowEmpty is false, the required values
+     * must also evaluate to a non-empty value to validate.
+     *
+     * Use the Validator::setThrownException() method to set which type of exception is thrown.
+     *
+     * This method always returns true, but throws an exception when the value is invalid.
+     *
+     * @param  array $values       The values to validate
+     * @param  array $required     List of keys
+     * @param  bool  $allowEmpty   Are empty values acceptable?
+     * @return bool
+     * @throws Exception When a required value is missing
+     */
+    public function validateRequired(array $values, array $required, $allowEmpty = false);
 } 
