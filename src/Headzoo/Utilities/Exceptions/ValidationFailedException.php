ff4fd5b5fab58d53a579135d3905c6585ad2baa5
headzoo <sean@headzoo.io>
2014-03-24 23:33:01 -0400

    Initial import

---
+++ src/Headzoo/Utilities/Exceptions/ValidationFailedException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Utilities\Exceptions;
+
+/**
+ * Thrown by the Validation class when a validation fails.
+ */
+class ValidationFailedException
+    extends UnexpectedValueException {}
