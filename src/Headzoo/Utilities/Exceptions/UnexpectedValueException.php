ad556cbbbf46e844ea8130879324e693e7e905d6
headzoo <sean@headzoo.io>
2014-03-24 23:31:42 -0400

    Initial import

---
+++ src/Headzoo/Utilities/Exceptions/UnexpectedValueException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Utilities\Exceptions;
+
+/**
+ * Thrown if a value does not match with a set of values.
+ */
+class UnexpectedValueException
+    extends \UnexpectedValueException {}
