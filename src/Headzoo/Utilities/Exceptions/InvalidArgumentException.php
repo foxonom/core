e41623941e753997527281fbd4e193d73408e79b
headzoo <sean@headzoo.io>
2014-03-26 18:15:36 -0400

    Initial import

---
+++ src/Headzoo/Utilities/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Headzoo\Utilities\Exceptions;
+
+/**
+ * Thrown if an argument is not of the expected type.
+ */
+class InvalidArgumentException
+    extends \InvalidArgumentException {}
