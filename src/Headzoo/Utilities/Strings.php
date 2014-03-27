0d9a8984ca85af2f085f9a18ee2abcff63f65083
headzoo <sean@headzoo.io>
2014-03-27 00:35:44 -0400

    Added a ton of methods to the Strings class

--- src/Headzoo/Utilities/Strings.php
+++ src/Headzoo/Utilities/Strings.php
@@ -3,9 +3,17 @@ namespace Headzoo\Utilities;
 
 /**
  * Contains static methods for working with strings.
+ * 
+ * The methods of this class are normalized, in that the string to operate on
+ * is always the first argument.
  */
 class Strings
 {
+    /**
+     * Default character set when not defined in the php.ini
+     */
+    const DEFAULT_CHAR_SET = "ISO-8859-1";
+    
     /**
      * Use lower case letters in random strings
      */
@@ -26,6 +34,53 @@ class Strings
      */
     const CHARS_PUNCTUATION  = 0x8;
 
+    /**
+     * Transform to upper case
+     */
+    const TR_UPPER = 1;
+
+    /**
+     * Transform to lower case
+     */
+    const TR_LOWER = 2;
+
+    /**
+     * Transform the first character of each word in a string, and the rest to lower case
+     */
+    const TR_TITLE = 3;
+
+    /**
+     * Transform the first character in a sentence into uppercase
+     */
+    const TR_UC_FIRST = 4;
+
+    /**
+     * Transform the first character in a sentence to lower case
+     */
+    const TR_LC_FIRST = 5;
+
+    /**
+     * Transform a string from CamelCaseText to underscore_text
+     */
+    const TR_UNDERSCORE = 6;
+
+    /**
+     * Transform a string from underscore_text to CamelCaseText
+     */
+    const TR_CAMEL_CASE = 7;
+    
+    /**
+     * The character set to use
+     * @var string
+     */
+    private static $char_set = null;
+
+    /**
+     * Whether to use the mbstring extension
+     * @var bool
+     */
+    private static $use_mbstring = false;
+    
     /**
      * Lower case letters, used to generate random strings
      * @var string
@@ -50,6 +105,115 @@ class Strings
      */
     private static $chars_punctuation = "!@#$%^&*()+=-_,.?{}[]<>";
 
+    /**
+     * Name of the mbstring extension
+     * 
+     * Used for testing purposes. The value should not be changed unless testing.
+     * 
+     * @var string
+     */
+    public static $__mbstring_extension_name = "mbstring";
+
+    /**
+     * Sets whether to use the "mbstring" extension for string operations
+     * 
+     * The "mbstring" extension must be loaded when set to true. An exception is thrown if the extension
+     * is not loaded. When set to false, the mbstring extension will not be used even if the
+     * extension is loaded.
+     *
+     * When turned on with true, the character set used by methods of this class will be set to the return
+     * value of ::getDefaultCharacterSet(). Use the ::setCharacterSet() method to set a different character set.
+     * See the ::getDefaultCharacterSet() method for more information.
+     * 
+     * @param bool $use_mbstring Whether to use the multi-byte extension
+     * @throws Exceptions\RuntimeException If the "mbstring" extension is not loaded
+     */
+    public static function setUseMultiByte($use_mbstring)
+    {
+        if ($use_mbstring) {
+            if (!extension_loaded(self::$__mbstring_extension_name)) {
+                throw new Exceptions\RuntimeException(
+                    sprintf(
+                        "The '%s' extension must be enabled.",
+                        self::$__mbstring_extension_name
+                    )
+                );
+            }
+            self::$use_mbstring = true;
+            if (!self::$char_set) {
+                self::$char_set = self::getDefaultCharacterSet();
+            }
+        } else {
+            self::$use_mbstring = false;
+        }
+    }
+    
+    /**
+     * Sets the character set this class uses for string operations
+     *
+     * The value of $char_set must be a valid character set name as defined by mb_list_encodings(), but the
+     * name may use any case and omit dashes. For example these are all valid character set
+     * names: "UTF-8", "utf-8", "UTF8", "utf8". The name of the encoding will be converted to it's proper
+     * name.
+     *
+     * Note: The return value from ::getDefaultCharSet() will be used when $char_set is empty. See that method
+     * for more information.
+     *
+     * Calling this method with any value other than "ASCII", "ISO-8859-1", empty, or the return value
+     * from ::getDefaultCharacterSet() will automatically call ::setUseMultiByte(true) to turn on the use of
+     * the "mbstring" extension. See the documentation on ::setUseMultiByte() for more information.
+     * 
+     * Note: This method does not change php's default internal encoding, eg by calling mb_internal_encoding().
+     * It only affects the encoding used when calling methods of this class.
+     * 
+     * @param string $char_set The name of the character encoding
+     * @throws Exceptions\InvalidArgumentException If $encoding is not a valid encoding name
+     * @throws Exceptions\RuntimeException If the "mbstring" extension is not loaded
+     */
+    public static function setCharacterSet($char_set)
+    {
+        $default   = self::getDefaultCharacterSet();
+        $char_set  = $char_set ? strtoupper($char_set) : $default;
+        $available = array_map("strtoupper", mb_list_encodings());
+        $is_valid  = false;
+        foreach($available as $a) {
+            if ($a === $char_set) {
+                $is_valid = true;
+                break;
+            } else if (str_replace("-", "", $a) === $char_set) {
+                $char_set = $a;
+                $is_valid = true;
+                break;
+            }
+        }
+        if (!$is_valid) {
+            throw new Exceptions\InvalidArgumentException(
+                "Value '{$char_set}' is not a valid character set name."
+            );
+        }
+        
+        $mb_required = ["ASCII", "ISO-8859-1", $default];
+        if (!self::$use_mbstring && !in_array($char_set, $mb_required)) {
+            self::setUseMultiByte(true);
+        }
+        
+        self::$char_set = $char_set;
+    }
+
+    /**
+     * Returns the default character set used for string operations
+     * 
+     * Returns the value of the "default_charset" php.ini directive, or ::DEFAULT_CHAR_SET when
+     * php has not been configured to use a default character set.
+     * 
+     * @return string
+     */
+    public static function getDefaultCharacterSet()
+    {
+        $default = ini_get("default_charset");
+        return strtoupper($default ? $default : self::DEFAULT_CHAR_SET);
+    }
+    
     /**
      * Simply wraps a string in quote characters
      * 
@@ -135,44 +299,345 @@ class Strings
     /**
      * Transforms a string with CamelCaseText into a string with underscore_text
      * 
+     * Note: The method also treats dashes "-" as underscores.
+     * 
      * Example:
      * ```php
-     * $str = "CamelCaseString";
-     * $str = Strings::regexCamelCaseToUnderscore($str);
-     * var_dum($str);
-     * 
-     * // Outputs:
-     * // string(17) "camel_case_string"
+     * echo Strings::camelCaseToUnderscore("CamelCaseString");
+     * // Outputs: "camel_case_string"
+     *
+     * echo Strings::camelCaseToUnderscore("MaryHadALittleLamb");
+     * // Outputs: "mary_had_a_little_lamb"
      * ```
      * 
      * @param  string $str The string to transform
      * @return string
      */
-    public static function transformCamelCaseToUnderscore($str)
+    public static function camelCaseToUnderscore($str)
     {
-        return strtolower(preg_replace("/([a-z])([A-Z])/", "\\1_\\2", $str));
+        $under = "";
+        $chars = self::chars($str);
+        $lower = false;
+        $upper = false;
+        foreach($chars as $char) {
+            if ("_" !== $char && "-" !== $char) {
+                if (self::startsUpper($char)) {
+                    $under .= ($lower || $upper) ? "_{$char}" : $char;
+                    $lower  = false;
+                    $upper  = true;
+                } else {
+                    $under .= $char;
+                    $lower  = true;
+                    $upper  = false;
+                }
+            }
+        }
+        
+        return self::toLower($under);
     }
 
     /**
      * Transforms a string with underscore_text into a string with CamelCaseText
      *
+     * Note: The method also treats dashes "-" as underscores.
+     * 
      * Example:
      * ```php
-     * $str = "camel_case_string";
-     * $str = Strings::regexCamelCaseToUnderscore($str);
-     * var_dum($str);
+     * echo Strings::underscoreToCamelCase("camel_case_string");
+     * // Outputs: "CamelCaseString"
      *
-     * // Outputs:
-     * // string(15) "CamelCaseString"
+     * $str = Strings::underscoreToCamelCase("camel-case-string");
+     * // Outputs: "CamelCaseString"
      * ```
      *
      * @param  string $str The string to transform
      * @return string
      */
-    public static function transformUnderscoreToCamelCase($str)
+    public static function underscoreToCamelCase($str)
     {
-        $str = str_replace("_", " ", $str);
-        $str = ucwords(strtolower($str));
-        return str_replace(" ", "", $str);
+        $str = self::replace($str, "_", " ");
+        $str = self::replace($str, "-", " ");
+        $str = self::title($str);
+        return self::replace($str, " ", "");
+    }
+
+    /**
+     * Returns whether the string starts with another string, using multi-byte functions when enabled
+     * 
+     * @param  string $str  The string to search
+     * @param  string $find The string to find
+     * @return bool
+     */
+    public static function startsWith($str, $find)
+    {
+        return self::$use_mbstring
+            ? mb_strpos($str, $find) === 0
+            : strpos($str, $find) === 0;
+    }
+
+    /**
+     * Returns whether the string ends with another string, using multi-byte functions when enabled
+     *
+     * @param  string $str  The string to search
+     * @param  string $find The string to find
+     * @return bool
+     */
+    public static function endsWith($str, $find)
+    {
+        if (self::$use_mbstring) {
+            $len = mb_strlen($find);
+            $is_ends = mb_substr($str, "-{$len}") === $find;
+        } else {
+            $len = strlen($find);
+            $is_ends = substr($str, "-{$len}") === $find;
+        }
+        
+        return $is_ends;
+    }
+
+    /**
+     * Returns whether the string starts with an upper case character, using multi-byte functions when enabled
+     * 
+     * @param  string $str The string to search
+     * @return bool
+     */
+    public static function startsUpper($str)
+    {
+        $char = self::sub($str, 0, 1);
+        return self::toUpper($char) === $char;
+    }
+
+    /**
+     * Returns whether the string starts with a lower case character, using multi-byte functions when enabled
+     *
+     * @param  string $str The string to search
+     * @return bool
+     */
+    public static function startsLower($str)
+    {
+        $char = self::sub($str, 0, 1);
+        return self::toLower($char) === $char;
+    }
+    
+    /**
+     * Replaces characters in a string, using multi-byte functions when enabled
+     * 
+     * @param  string $str The string to transform
+     * @param  string $search The string to find
+     * @param  string $replace The replacement string
+     * @return string
+     */
+    public static function replace($str, $search, $replace)
+    {
+        if (self::$use_mbstring) {
+            $search_len  = mb_strlen($search);
+            $replace_len = mb_strlen($replace);
+            $search_pos  = mb_strpos($str, $search);
+            while ($search_pos !== false) {
+                $str = mb_substr($str, 0, $search_pos) .
+                    $replace .
+                    mb_substr($str, $search_pos + $search_len);
+                $search_pos = mb_strpos($str, $search, $search_pos + $replace_len);
+            }
+        } else {
+            $str = str_replace($search, $replace, $str);
+        }
+        
+        return $str;
+    }
+
+    /**
+     * Returns the number of characters in the string, using multi-byte functions when enabled
+     * 
+     * @param  string $str The string to count
+     * @return int
+     */
+    public static function length($str)
+    {
+        return self::$use_mbstring
+            ? mb_strlen($str, self::$char_set)
+            : strlen($str);
+    }
+
+    /**
+     * Splits a string into individual characters, using multi-byte functions when enabled
+     * 
+     * @param  string $str The string to split
+     * @return array
+     */
+    public static function chars($str)
+    {
+        if (self::$use_mbstring) {
+            $chars = [];
+            for($i = 0, $l = self::length($str); $i < $l; $i++) {
+                $chars[] = mb_substr($str, $i, 1, self::$char_set);
+            }
+        } else {
+            $chars = str_split($str);
+        }
+        
+        return $chars;
+    }
+    
+    /**
+     * Transforms a string to lower case, using multi-byte functions when enabled
+     * 
+     * @param  string $str The string to transform
+     * @return string
+     */
+    public static function toUpper($str)
+    {
+        return self::$use_mbstring
+            ? mb_strtoupper($str, self::$char_set)
+            : strtoupper($str);
+    }
+
+    /**
+     * Transforms a string to upper case, using multi-byte functions when enabled
+     *
+     * @param  string $str The string to transform
+     * @return string
+     */
+    public static function toLower($str)
+    {
+        return self::$use_mbstring
+            ? mb_strtolower($str, self::$char_set)
+            : strtolower($str);
+    }
+
+    /**
+     * Transforms the first letter of each word to upper case, using multi-byte functions when enabled
+     *
+     * @param  string $str The string to transform
+     * @return string
+     */
+    public static function ucFirst($str)
+    {
+        if (self::$use_mbstring) {
+            $first = self::toUpper(self::sub($str, 0, 1));
+            $str   = $first . self::sub($str, 1);
+        } else {
+            $str   = ucfirst($str);
+        }
+        
+        return $str;
+    }
+
+    /**
+     * Transforms the first letter of each word to lower case, using multi-byte functions when enabled
+     *
+     * @param  string $str The string to transform
+     * @return string
+     */
+    public static function lcFirst($str)
+    {
+        if (self::$use_mbstring) {
+            $first = self::toLower(self::sub($str, 0, 1));
+            $str   = $first . self::sub($str, 1);
+        } else {
+            $str   = lcfirst($str);
+        }
+        
+        return $str;
+    }
+
+    /**
+     * Upper cases the first letter of each word in the string, using multi-byte functions when enabled
+     *
+     * @param  string $str The string to transform
+     * @return string
+     */
+    public static function title($str)
+    {
+        return self::$use_mbstring
+            ? mb_convert_case($str, MB_CASE_TITLE, self::$char_set)
+            : ucwords(strtolower($str));
+    }
+
+    /**
+     * Returns a portion of the string, using multi-byte functions when enabled
+     *
+     * @param  string $str   The string
+     * @param  int    $start The start position
+     * @param  int    $end   The end position
+     * @return string
+     */
+    public static function sub($str, $start, $end = null)
+    {
+        return self::$use_mbstring
+            ? mb_substr($str, $start, $end)
+            : substr($str, $start, $end);
+    }
+
+    /**
+     * Splits a string by regular expression, using multi-byte functions when enabled
+     *
+     * @param  string $str     The string to split
+     * @param  string $pattern The regular expression pattern
+     * @param  string $limit   If optional parameter limit is specified, it will be split in limit elements as maximum
+     * @return array
+     */
+    public static function split($str, $pattern, $limit = -1)
+    {
+        return self::$use_mbstring
+            ? mb_split($pattern, $str, $limit)
+            : preg_split($pattern, $str, $limit);
+    }
+
+    /**
+     * Transform a string
+     *
+     * This method wraps the functions ::toLower(), ::toUpper(), ::title(), ::ucFirst(), ::lcFirst(),
+     * ::camelCaseToUnderscore(), and ::underscoreToCamelCase()
+     * .
+     * The notable difference is strings are passed by reference to this method.
+     *
+     * The value of $transformation must be one of the Strings::TR constants, which map to the following methods:
+     *  ::TR_LOWER      - ::toLower().
+     *  ::TR_UPPER      - ::toUpper().
+     *  ::TR_TITLE      - ::title().
+     *  ::TR_UC_FIRST   - ::ucFirst().
+     *  ::TR_LC_FIRST   - ::lcFirst().
+     *  ::TR_UNDERSCORE - ::camelCaseToUnderscore()
+     *  ::TR_CAMEL_CASE - ::underscoreToCamelCase()
+     *
+     * @param string $str            The string to transform
+     * @param int    $transformation The transformation to apply
+     * @throws Exceptions\InvalidArgumentException When $transformation is not one of the ::TR constants
+     */
+    public static function transform(&$str, $transformation = self::TR_LOWER)
+    {
+        switch($transformation) {
+            case self::TR_LOWER:
+                $str = self::toLower($str);
+                break;
+            case self::TR_UPPER:
+                $str = self::toUpper($str);
+                break;
+            case self::TR_TITLE:
+                $str = self::title($str);
+                break;
+            case self::TR_UC_FIRST:
+                $str = self::ucFirst($str);
+                break;
+            case self::TR_LC_FIRST:
+                $str = self::lcFirst($str);
+                break;
+            case self::TR_UNDERSCORE:
+                $str = self::camelCaseToUnderscore($str);
+                break;
+            case self::TR_CAMEL_CASE:
+                $str = self::underscoreToCamelCase($str);
+                break;
+            default:
+                throw new Exceptions\InvalidArgumentException(
+                    sprintf(
+                        "Transformation argument %s must be one of the %s::TR constants.",
+                        __METHOD__,
+                        __CLASS__
+                    )
+                );
+                break;
+        }
     }
 }
--- tests/Headzoo/Utilities/StringsTest.php
+++ tests/Headzoo/Utilities/StringsTest.php
@@ -1,11 +1,68 @@
 <?php
 use Headzoo\Utilities\Strings;
 
+/**
+ * @coversDefaultClass Headzoo\Utilities\Strings
+ * @requires extension mbstring
+ */
 class StringsTest
     extends PHPUnit_Framework_TestCase
 {
     /**
-     * @covers Headzoo\Utilities\Strings::quote
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    public function setUp()
+    {
+        Strings::$__mbstring_extension_name = "mbstring";
+        Strings::setUseMultiByte(false);
+        Strings::setCharacterSet(null);
+    }
+    
+    /**
+     * @covers ::setUseMultiByte
+     */
+    public function testSetUseMultiByte()
+    {
+        Strings::setUseMultiByte(true);
+        Strings::setUseMultiByte(false);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @covers ::setUseMultiByte
+     * @expectedException Headzoo\Utilities\Exceptions\RuntimeException
+     */
+    public function testSetUseMultiByte_RuntimeException()
+    {
+        Strings::$__mbstring_extension_name = "mbstring-test";
+        Strings::setUseMultiByte(true);
+    }
+
+    /**
+     * @covers ::setCharacterSet
+     */
+    public function testSetCharacterSet()
+    {
+        Strings::setCharacterSet("ISO-8859-10");
+        Strings::setCharacterSet("UTF-8");
+        Strings::setCharacterSet("UTF8");
+        Strings::setCharacterSet("utf8");
+        Strings::setCharacterSet(null);
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @covers ::setCharacterSet
+     * @expectedException Headzoo\Utilities\Exceptions\InvalidArgumentException
+     */
+    public function testSetCharacterSet_InvalidArgumentException()
+    {
+        Strings::setCharacterSet("UTF-88");
+    }
+    
+    /**
+     * @covers ::quote
      */
     public function testQuote()
     {
@@ -20,7 +77,7 @@ class StringsTest
     }
     
     /**
-     * @covers Headzoo\Utilities\Strings::random
+     * @covers ::random
      */
     public function testRandom()
     {
@@ -32,36 +89,217 @@ class StringsTest
     }
 
     /**
- * @covers Headzoo\Utilities\Strings::transformCamelCaseToUnderscore
- */
-    public function testTransformCamelCaseToUnderscore()
+     * @covers ::startsWith
+     */
+    public function testStartsWith()
+    {
+        $this->assertTrue(Strings::startsWith("But Gollum, and the evil one", "But"));
+        $this->assertFalse(Strings::startsWith("But Gollum, and the evil one", "Gollum"));
+
+        Strings::setUseMultiByte(true);
+        $this->assertTrue(Strings::startsWith("But Gollum, and the evil one", "But"));
+        $this->assertFalse(Strings::startsWith("But Gollum, and the evil one", "Gollum"));
+    }
+
+    /**
+     * @covers ::endsWith
+     */
+    public function testEndsWith()
+    {
+        $this->assertTrue(Strings::endsWith("But Gollum, and the evil one", "one"));
+        $this->assertFalse(Strings::endsWith("But Gollum, and the evil one", "evil"));
+
+        Strings::setUseMultiByte(true);
+        $this->assertTrue(Strings::endsWith("But Gollum, and the evil one", "one"));
+        $this->assertFalse(Strings::endsWith("But Gollum, and the evil one", "evil"));
+    }
+
+    /**
+     * @covers ::length
+     */
+    public function testLength()
     {
         $this->assertEquals(
-            "camel_case_string",
-            Strings::transformCamelCaseToUnderscore("CamelCaseString")
+            6,
+            Strings::length("Gollum")
         );
+        
+        Strings::setUseMultiByte(true);
         $this->assertEquals(
-            "ca_mel_ca_se_st_ring",
-            Strings::transformCamelCaseToUnderscore("Ca_melCa_seSt_ring")
-        );
-        $this->assertEquals(
-            "camel_case_string",
-            Strings::transformCamelCaseToUnderscore("Camel_Case_String")
+            6,
+            Strings::length("Gollum")
         );
     }
 
     /**
-     * @covers Headzoo\Utilities\Strings::transformUnderscoreToCamelCase
+     * @covers ::chars
+     */
+    public function testChars()
+    {
+        $this->assertEquals(
+            ["G", "o", "l", "l", "u", "m"],
+            Strings::chars("Gollum")
+        );
+        
+        Strings::setUseMultiByte(true);
+        $this->assertEquals(
+            ["G", "o", "l", "l", "u", "m"],
+            Strings::chars("Gollum")
+        );
+    }
+
+    /**
+     * @covers ::startsUpper
+     */
+    public function testStartsUpper()
+    {
+        $this->assertTrue(Strings::startsUpper("Gollum"));
+        $this->assertFalse(Strings::startsUpper("gollum"));
+        
+        Strings::setUseMultiByte(true);
+        $this->assertTrue(Strings::startsUpper("Gollum"));
+        $this->assertFalse(Strings::startsUpper("gollum"));
+    }
+
+    /**
+     * @covers ::startsLower
+     */
+    public function testStartsLower()
+    {
+        $this->assertTrue(Strings::startsLower("gollum"));
+        $this->assertFalse(Strings::startsLower("Gollum"));
+
+        Strings::setUseMultiByte(true);
+        $this->assertTrue(Strings::startsLower("gollum"));
+        $this->assertFalse(Strings::startsLower("Gollum"));
+    }
+
+    /**
+     * @covers ::camelCaseToUnderscore
+     */
+    public function testCamelCaseToUnderscore()
+    {
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("CamelCaseString")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Ca_melCa_seSt_ring")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Camel_Case_String")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Camel-Case-String")
+        );
+        $this->assertEquals(
+            "mary_had_a_little_lamb",
+            Strings::camelCaseToUnderscore("MaryHadALittleLamb")
+        );
+        
+        Strings::setUseMultiByte(true);
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("CamelCaseString")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Ca_melCa_seSt_ring")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Camel_Case_String")
+        );
+        $this->assertEquals(
+            "camel_case_string",
+            Strings::camelCaseToUnderscore("Camel-Case-String")
+        );
+        $this->assertEquals(
+            "mary_had_a_little_lamb",
+            Strings::camelCaseToUnderscore("MaryHadALittleLamb")
+        );
+    }
+
+    /**
+     * @covers ::underscoreToCamelCase
      */
     public function testTransformUnderscoreToCamelCase()
     {
         $this->assertEquals(
             "CamelCaseString",
-            Strings::transformUnderscoreToCamelCase("camel_case_string")
+            Strings::underscoreToCamelCase("camel_case_string")
         );
         $this->assertEquals(
             "CamelCaseString",
-            Strings::transformUnderscoreToCamelCase("Camel_Case_String")
+            Strings::underscoreToCamelCase("Camel_Case_String")
         );
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("Camel-Case-String")
+        );
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("Camel__Case__String")
+        );
+
+        Strings::setUseMultiByte(true);
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("camel_case_string")
+        );
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("Camel_Case_String")
+        );
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("Camel-Case-String")
+        );
+        $this->assertEquals(
+            "CamelCaseString",
+            Strings::underscoreToCamelCase("Camel__Case__String")
+        );
+    }
+
+    /**
+     * @covers ::transformCase
+     * @dataProvider providerTransform
+     */
+    public function testTransform($str, $case, $expected)
+    {
+        $orig = $str;
+        Strings::transform($str, $case);
+        $this->assertEquals(
+            $expected,
+            $str
+        );
+
+        Strings::setUseMultiByte(true);
+        Strings::transform($orig, $case);
+        $this->assertEquals(
+            $expected,
+            $orig
+        );
+    }
+
+    /**
+     * Data provider for testTransform
+     * 
+     * @return array
+     */
+    public function providerTransform()
+    {
+        return [
+            ["But Gollum, and the evil one",     Strings::TR_LOWER,      "but gollum, and the evil one"],
+            ["Leaves are falling all around",    Strings::TR_UPPER,      "LEAVES ARE FALLING ALL AROUND"],
+            ["It's timE I waS on my waY",        Strings::TR_TITLE,      "It's Time I Was On My Way"],
+            ["sometimes I grow so tired",        Strings::TR_UC_FIRST,   "Sometimes I grow so tired"],
+            ["But Gollum, and the evil one",     Strings::TR_LC_FIRST,   "but Gollum, and the evil one"],
+            ["MaryHadALittleLamb",               Strings::TR_UNDERSCORE, "mary_had_a_little_lamb"],
+            ["mary_had_a_little_lamb",           Strings::TR_CAMEL_CASE, "MaryHadALittleLamb"]
+        ];
     }
 }
