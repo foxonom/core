43b2e345993e7c402fe0b805ed5e5bd36b9349cc
headzoo <sean@headzoo.io>
2014-03-30 02:38:02 -0400

    Committing

---
+++ tests/Headzoo/Core/errors/error.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * User: headz
+ * Date: 3/30/14
+ * Time: 12:53 AM
+ */ 
---
+++ tests/Headzoo/Core/errors/exception.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * User: headz
+ * Date: 3/30/14
+ * Time: 1:03 AM
+ */ 
