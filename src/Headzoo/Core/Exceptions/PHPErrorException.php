40941beefd3a2358d21071db06ad16bc766d354e
headzoo <sean@headzoo.io>
2014-04-01 13:13:53 -0400

    PHPErrorException now extends PHPException

--- src/Headzoo/Core/Exceptions/PHPErrorException.php
+++ src/Headzoo/Core/Exceptions/PHPErrorException.php
@@ -2,24 +2,7 @@
 namespace Headzoo\Core\Exceptions;
 
 /**
- * Thrown when PHP triggers an error.
+ * Thrown when PHP triggers an E_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR, E_CORE_ERROR or E_COMPILE_ERROR.
  */
 class PHPErrorException
-    extends Exception
-{
-    /**
-     * Constructor
-     * 
-     * @param string     $message   The error message
-     * @param int        $code      The error code
-     * @param string     $file      The file where the error occurred
-     * @param int        $line      The line where the error occurred
-     * @param \Exception $prev      Previous exception
-     */
-    public function __construct($message, $code, $file, $line, \Exception $prev = null)
-    {
-        parent::__construct($message, $code, $prev);
-        $this->file = $file;
-        $this->line = $line;
-    }
-}
+    extends PHPException {}
